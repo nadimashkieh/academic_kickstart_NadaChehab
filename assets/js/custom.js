1ef4f9b7fb0d0a0b433707cf9160c02e8f6f3f7a
nadimashkieh <59630860+nadimashkieh@users.noreply.github.com>
2021-04-19 23:12:48 -0400

    sss

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+<script> 
+	const mySound = document.getElementById("sound"); 
+	const startStopButton = document.getElementById("startStopSound"); 
+	var toggleSound = 0;
+
+	startStopButton.addEventListener("click", function(){ 
+	if( toggleSound == 0 )
+	{
+		mySound.play();
+		toggleSound = 1;
+	}
+	else
+	{
+		mySound.pause();
+		toggleSound = 0;
+	} })
+
+</script> 
+
