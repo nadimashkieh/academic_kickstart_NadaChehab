1cae68101cb967cf57e6eda3b4ca1733424406da
nadimashkieh <59630860+nadimashkieh@users.noreply.github.com>
2021-04-26 13:47:52 -0400

    aab

--- assets/js/my_sound.js
+++ assets/js/my_sound.js
@@ -1,10 +1,7 @@
-<script> 
-	const mySound = document.getElementById("sound"); 
-	const startStopButton = document.getElementById("startStopSound"); 
-	var toggleSound = 0;
 
-	startStopButton.addEventListener("click", function(){})
+const mySound = document.getElementById("sound"); 
+const startStopButton = document.getElementById("startStopSound"); 
+var toggleSound = 0;
 
-
-</script> 
+startStopButton.addEventListener("click", function(){ if( toggleSound == 0 ){mySound.play();toggleSound = 1;}else{mySound.pause();toggleSound = 0;}})
 
